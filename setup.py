8f9c959111c63666bddb88c8af497cba77d58409
xEpsi <66256489+xEpsi@users.noreply.github.com>
2022-02-05 14:01:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from os import system
 print("Starting setup\n")
-system('cmd /c "pip install pyautogui"')
-system('cmd /c "pip install selenium"')
-system('cmd /c "pip install colorama"')
-print("\nSuccessfully imported the required modules. You can now start the program.")
+try:
+  system('cmd /c "pip install pyautogui"')
+  system('cmd /c "pip install selenium"')
+  system('cmd /c "pip install colorama"')
+  print("\nSuccessfully imported the required modules. You can now start the program.")
+except:
+  print("Error: Failed to install the modules. Try installing them by hand.")
